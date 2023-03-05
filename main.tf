8b8c1f6f54e646f0914893317f74c2eba6c1ca6c
Ahmed <ae70856@gmail.com>
2023-03-05 23:05:50 +0200

    third commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "mymodule" {
-  source  = "https://github.com/AhmedElsayed970/Tier2-app-module.git"
+  source  = "github.com/AhmedElsayed970/Tier2-app-module.git"
   vpc     = var.vpc
   subnets = var.subnets
 }
--- var.tf
+++ var.tf
@@ -1,9 +1,10 @@
+#This data object is going to be holding the vpc in aws
 variable "vpc" {}
 data "aws_vpc" "vpc" {
   id = var.vpc
 }
 
 variable "subnets" {
-  type = map
+  type    = map(any)
   default = {}
 }
